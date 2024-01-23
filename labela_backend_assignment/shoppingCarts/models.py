75ee9262befc310e9d02672fc279f289dc194a4a
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-01-23 06:08:51 +0530

    Name change Carparts to products

--- labela_backend_assignment/shoppingCarts/models.py
+++ labela_backend_assignment/shoppingCarts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from users.models import User
 from carParts.models import CarPart
+from products.models import Product
 
 
 class ShoppingCart(models.Model):
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
-    CarPartID = models.ForeignKey(CarPart, on_delete=models.CASCADE)
-    #quantity = models.PositiveIntegerField()
-
+    ProductID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # CarPartID = models.ForeignKey(CarPart, on_delete=models.CASCADE)
+    # quantity = models.PositiveIntegerField()
--- labela_backend_assignment/shoppingCarts/serializer.py
+++ labela_backend_assignment/shoppingCarts/serializer.py
@@ -2,8 +2,8 @@ from carParts.models import CarPart
 from shoppingCarts.models import ShoppingCart
 from rest_framework import serializers
 
+
 class ShoppingCartsSerializer(serializers.ModelSerializer):
     class Meta:
-        model =  ShoppingCart
-        fields = ['id','UserID','CarPartID']
-
+        model = ShoppingCart
+        fields = ['id', 'UserID', 'ProductID']
