a9b3cdef35a1288d4deacb92e6fe107a807322f1
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-01-25 05:33:37 +0530

    Syntax error

--- labela_backend_assignment/products/process/productProcess.py
+++ labela_backend_assignment/products/process/productProcess.py
@@ -10,12 +10,12 @@ from rest_framework import status
 
 def HandleGetProcess(inputData):
 
-    fetched_data = Product.objects.filter(id=inputData['id'])
+    fetched_data = Product.objects.filter(id=inputData["id"])
     serializer = ProductsSerializer(fetched_data, many=True)
     if not fetched_data.exists():
-        return ({'message': 'Product not found'}, status.HTTP_404_NOT_FOUND)
+        return ({"message": "Product not found"}, status.HTTP_404_NOT_FOUND)
 
-    return ({'message': 'Successfully retrieved all product data',  'data': serializer.data}, status.HTTP_200_OK)
+    return ({"message": "Successfully retrieved all product data", "data": serializer.data}, status.HTTP_200_OK)
 
 
 def HandlePostProcess(inputData):
@@ -24,17 +24,17 @@ def HandlePostProcess(inputData):
         raise Exception(serializer.errors)
 
     serializer.save()
-    return ({'message': 'Successfully added the product data',  'data': inputData}, status.HTTP_200_OK)
+    return ({"message": "Successfully added the product data", "data": inputData}, status.HTTP_200_OK)
 
 
 def HandlePutProcess():
-    return 'HandlePutProcess called'
+    return "HandlePutProcess called"
 
 
 def HandleDeleteProcess(inputData):
-    fetched_data = Product.objects.filter(id=inputData['id'])
+    fetched_data = Product.objects.filter(id=inputData["id"])
     if not fetched_data.exists():
-        return ({'message': 'Product not found'}, status.HTTP_404_NOT_FOUND)
+        return ({"message": "Product not found"}, status.HTTP_404_NOT_FOUND)
     fetched_data.delete()
 
-    return ({'message': f'Successfully deleted the product of id {inputData['id']}',  'data': inputData}, status.HTTP_200_OK)
+    return ({"message": f"Successfully deleted the product of id {inputData['id']}", "data": inputData}, status.HTTP_200_OK)
